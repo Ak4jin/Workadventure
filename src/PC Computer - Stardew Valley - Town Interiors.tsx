78e1d75ca1c08ffeffe38b42e5f74ec2e7840c56
Ak4jin <Ak4jin@outlook.de>
2024-08-12 14:00:13 +0200

    test 2

---
+++ src/PC Computer - Stardew Valley - Town Interiors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="PC Computer - Stardew Valley - Town Interiors" tilewidth="16" tileheight="16" tilecount="3488" columns="32">
+ <image source="../../../../Downloads/PC Computer - Stardew Valley - Town Interiors.png" width="512" height="1744"/>
+</tileset>
---
+++ src/Tile_20-128x128.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Tile_20-128x128" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../../../../Downloads/SBS - Tiny Texture Pack 2 - 128x128/128x128/Tile/Tile_20-128x128.png" width="128" height="128"/>
+</tileset>
