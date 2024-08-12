80b234db0a6b723dc899d20dd57ba212507914fa
Ak4jin <Ak4jin@outlook.de>
2024-08-12 15:06:08 +0200

    TEST 3

--- office-skizze.pdf.tsx
+++ office-skizze.pdf.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="office-skizze.pdf" tilewidth="16" tileheight="16" tilecount="3234" columns="77">
- <image source="../../../../Downloads/Screenshot 2024-08-01 at 16-06-37 Web 1920 – 1-1.pdf.png" width="1240" height="685"/>
+ <image source="tilesets/Screenshot 2024-08-01 at 16-06-37 Web 1920 – 1-1.pdf.png" width="1240" height="685"/>
 </tileset>
